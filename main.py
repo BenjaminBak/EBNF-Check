978e88e30eefb1b7ac321c2bdcb5fd4c1965e27c
Yosheeesh <34773507+Yosheeesh@users.noreply.github.com>
2021-02-27 21:39:03 +0100

    test for version control

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Opening/Reading a text file content
+# Test 123
 ntcharlist = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
                     'k',
                     'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E',
